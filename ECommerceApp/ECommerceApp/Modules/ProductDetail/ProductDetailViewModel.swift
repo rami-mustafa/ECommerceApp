865289e9e49a270fea1349916e882d14a95cebb1
Rami Mustafa <ramimustafa77@gmail.com>
2024-12-22 00:56:40 +0300

    ProductDetailViewController with detailed UI setup

---
+++ ECommerceApp/ECommerceApp/Modules/ProductDetail/ProductDetailViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  ProductDetailViewModel.swift
+//  ECommerceApp
+//
+//  Created by Rami Mustafa on 21.12.24.
+//
+
+import Foundation
+
+protocol ProductDetailViewDelegate: AnyObject {
+    func updateImage(_ imageUrl: String)
+    func updateTitle(_ title: String)
+    func updateDescription(_ description: String)
+    func updatePrice(_ price: String)
+}
+
+final class ProductDetailViewModel {
+    weak var delegate: ProductDetailViewDelegate?
+    private let product: Product
+
+    init(product: Product) {
+        self.product = product
+    }
+
+    func viewDidLoad() {
+        delegate?.updateImage(product.image ?? "")
+        delegate?.updateTitle(product.safeName)
+        delegate?.updateDescription(product.description ?? "No description available.")
+        delegate?.updatePrice("\(product.safePrice)")
+    }
+}
---
+++ ECommerceApp/ECommerceApp/Utilities/Extensions/String+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  ECommerceApp
+//
+//  Created by Rami Mustafa on 22.12.24.
+//
+
+import Foundation
+
+extension String {
+    var tl: String {
+        return self + " ₺"
+    }
+}
